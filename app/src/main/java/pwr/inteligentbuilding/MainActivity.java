44b3e39b74100001ef5fed4764d4274fc7208275
JakubManka <49609640+JakubManka@users.noreply.github.com>
2021-07-25 18:04:14 +0200

    :sparkles: add feature to turn on and off lights

--- app/src/main/java/pwr/inteligentbuilding/MainActivity.java
+++ app/src/main/java/pwr/inteligentbuilding/MainActivity.java
@@ -3,12 +3,71 @@ package pwr.inteligentbuilding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    Map<SwitchMaterial, Boolean> lights = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        lights.put(findViewById(R.id.oswietlenie, false);
+        lights.put((SwitchMaterial) findViewById(R.id.oswietlenie), false);
+        lights.put((SwitchMaterial) findViewById(R.id.kuchnia), false);
+        lights.put((SwitchMaterial) findViewById(R.id.sypialnia), false);
+        lights.put((SwitchMaterial) findViewById(R.id.salon), false);
+
+
+        lights.forEach((light, checked) -> light.setOnCheckedChangeListener(this));
+
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        lights.put((SwitchMaterial) findViewById(buttonView.getId()), isChecked);
+
+        updateLightsView(buttonView, isChecked);
+
+    }
+
+    private void updateLightsView(CompoundButton buttonView, boolean isChecked) {
+
+        if (buttonView.getId() == R.id.oswietlenie && !isAnyLightChecked()) {
+
+            lights.replaceAll((lights, clicked) -> clicked = isChecked);
+
+        } else if (buttonView.getId() == R.id.oswietlenie && !isChecked) {
+            lights.replaceAll((lights, clicked) -> clicked = isChecked);
+
+        } else if (isAnyLightChecked()) {
+            lights.put((SwitchMaterial) findViewById(R.id.oswietlenie), true);
+        }
+
+        lights.forEach((light, clicked) -> {
+            light.setChecked(clicked);
+        });
+    }
+
+    private boolean isAnyLightChecked() {
+        boolean[] isClicked = {false};
+        lights.forEach((lights, clicked) -> {
+            if (lights.getId() != R.id.oswietlenie && clicked) {
+                isClicked[0] = true;
+            }
+        });
+        return isClicked[0];
     }
 }
